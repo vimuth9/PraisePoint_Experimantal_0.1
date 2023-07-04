99c33305b7b89571cac14ba2e91b2d0f826fb097
vimuth9 <vimuthwic3@gmail.com>
2023-07-04 10:20:08 +0530

    update of tool tip and the start of tranpose java script

--- JS/01_TransposeM.js
+++ JS/01_TransposeM.js
@@ -1,10 +1,15 @@
+
+function orginalKeyHandler() {
+    
+}
+
 function transpose () {
 
-const C = (1:C,2:C#,3:D,4:D#,5:E,6:F,7:F#,8:G,9:G#,10:A,11:A#,12:B)
-
-  
-  
-  
-  
-  
 }
+
+function orginalKeyToNumberConverter () {
+
+}
+ function NumberToKeyChanged () {
+
+ }
