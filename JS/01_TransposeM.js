502ab2f5a3cc20a9fb8a576974596dcffff98446
vimuth9 <vimuthwic3@gmail.com>
2023-07-07 23:33:14 +0530

    Big Update Last

--- JS/01_TransposeM.js
+++ JS/01_TransposeM.js
@@ -6,23 +6,38 @@ function PraisePoint_Chords() {
     }
     console.log("Show Chords Transpose Controlls")
 }
+
 function PraisePoint_Lyrics() {
     Transpose_Controller.style.display = "none";
     for (const Chord_Line of Chord_Lines) {
         Chord_Line.style.display = "none";
     }
     console.log("Hide Chords Transpose Controlls")
-    
+
 
 }
 
 // ```````````````````` Transposer Javascript ````````````````````````
-function transposeControler() {
-}
-function orginalKeyHandler() {
+//
+function preTranspose() {
+    var _currentKey = _orginalKey;
+    currentKeyToNum();
+    console.log( "currentkey is " + _currentKeyNum )
+
 }
 
-function transpose () {
-    // console.log("keySelector Changed");
+// currentkey to currentKeyNumber - chart and 
+function currentKeyToNum() {
+    console.log("current key is" + _curentKey)
+    console.log("current key is" + _currentKeyNum )
+
+}
+function chordNumberToChords() {
     
+}
+
+//Transposing algorithum
+function transpose() {
+    console.log("keySelector Changed Transpose");
+
 }
