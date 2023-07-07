5e3a8fa5e8407598d7b13999cf4a813df18b9357
vimuth9 <vimuthwic3@gmail.com>
2023-07-07 14:19:22 +0530

    update work

--- JS/01_TransposeM.js
+++ JS/01_TransposeM.js
@@ -1,17 +1,10 @@
-function transposeControler(params) {
-    
+//
+function transposeControler() {
 }
 function orginalKeyHandler() {
-    
 }
 
 function transpose () {
-
-const C = (1:C,2:C#,3:D,4:D#,5:E,6:F,7:F#,8:G,9:G#,10:A,11:A#,12:B)
-
-  
-  
-  
-  
-  
+    // console.log("keySelector Changed");
+    
 }
