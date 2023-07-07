c7392f23a1998d9ad67ddec86f59a58a72e5dd3d
vimuth9 <vimuthwic3@gmail.com>
2023-07-07 16:25:15 +0530

    Big Update of the Chord and Lyrics Nav

--- JS/01_TransposeM.js
+++ JS/01_TransposeM.js
@@ -1,4 +1,19 @@
-//
+// NAV button to show with Chords and Show Lyrics
+function PraisePoint_Chords() {
+    for (const Chord_Line of Chord_Lines) {
+        Chord_Line.style.display = "flex";
+    }
+    console.log("Show Chords")
+}
+function PraisePoint_Lyrics() {
+    for (const Chord_Line of Chord_Lines) {
+        Chord_Line.style.display = "none";
+    }
+    console.log("Hide Chords")
+
+}
+
+//Transposer
 function transposeControler() {
 }
 function orginalKeyHandler() {
