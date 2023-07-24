9d9fee8b405e131b4fb10962c00beabed595aff6
vimuth9 <vimuthwic3@gmail.com>
2023-07-24 19:57:26 +0530

    update 23.24

--- JS/01_TransposeM.js
+++ JS/01_TransposeM.js
@@ -4,13 +4,38 @@ function preTranspose () {
 }
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function transpose() {
-  
+  console.log("_currentKey stays as this during the transposing cycle =" + _currentKey);
+  const keySelectorValue = keySelector.value;   // the key to be changed
+  toWhichKey(keySelectorValue);
+  console.log("_NewKey is =" + _NewKey);
+  console.log(" ~~~ Transposing starts from here");
+
+
+
+
+
+
+  //after transposing is finised
+  console.log(" ~~~ Transposing finished");
+  _currentKey = _NewKey ;
+  console.log("so the _currentKey is =" + _currentKey);
+
+
 }
+function toWhichKey(keySelectorValue) {
+  if (keySelectorValue === "orginalKey") {
+    _NewKey = _orginalKey;
+  } else {
+    _NewKey = keySelector.value;
+  } 
+}
+
 //`````````````````````````````````````````````````
 function chordToNumber () {
-
+  
 }
 function numberToChord() {
   
 }
-//`````````````````````````````````````````````````
+/*`````````````````````````````````````````````````
+*/
