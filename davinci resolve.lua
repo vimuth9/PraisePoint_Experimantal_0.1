c864ce98a806801b9bb04c7f4efe1baaefd10f76
vimuth9 <vimuthwic3@gmail.com>
2023-08-30 13:12:01 +0530

    ff

---
+++ davinci resolve.lua
@@ -0,0 +1,12 @@
+{
+	Tools = ordered() {
+		ABLayer_Transform = Transform {
+			CtrlWZoom = false,
+			NameSet = true,
+			ViewInfo = OperatorInfo { Pos = { -880, 356.09 } },
+			Colors = { TileColor = { R = 0.584313725490196, G = 0.294117647058824, B = 0.803921568627451 }, },
+			UserControls = ordered() { AboveBackground_Layer_Type = { { MBTNC_AddButton = "1" }, { MBTNC_AddButton = "2" }, { MBTNC_AddButton = "3" }, { MBTNC_AddButton = "4" }, { MBTNC_AddButton = "5" }, { MBTNC_AddButton = "6" }, { MBTNC_AddButton = "7" }, { MBTNC_AddButton = "8" }, { MBTNC_AddButton = "9" }, { MBTNC_AddButton = "10" }, { MBTNC_AddButton = "11" }, { MBTNC_AddButton = "12" }, { MBTNC_AddButton = "13" }, { MBTNC_AddButton = "14" }, INP_MaxAllowed = 1000000, INP_Integer = false, INPID_InputControl = "MultiButtonControl", MBTNC_ShowBasicButton = false, INP_MaxScale = 1, INP_MinScale = 0, INP_MinAllowed = -1000000, LINKID_DataType = "Number", MBTNC_ShowName = false, MBTNC_StretchToFit = false, MBTNC_ShowToolTip = false, LINKS_Name = "AboveBackground_Layer_Type", }, Size = { INP_MaxAllowed = 1000000, INP_Integer = false, INPID_InputControl = "SliderControl", INP_MaxScale = 5, INP_Default = 1, PC_ControlID = 0, INPID_PreviewControl = "RectangleControl", INP_MinAllowed = 0, LINKID_DataType = "Number", PC_ControlGroup = 2, INP_MinScale = 0, ICD_Center = 1, LINKS_Name = "Size", }, Angle = { INP_MaxAllowed = 1000000, INP_Integer = false, INPID_InputControl = "ScrewControl", IC_Steps = 3601, INP_MaxScale = 360, INP_Default = 0, INPID_PreviewControl = "AngleControl", INP_MinAllowed = -1000000, LINKID_DataType = "Number", INP_MinScale = 0, LINKS_Name = "Angle", }, Center = { INPID_PreviewControl = "CrosshairControl", LINKID_DataType = "Point", INPID_InputControl = "OffsetControl", OFCD_DisplayYScale = 1, OFCD_DisplayXScale = 1, CHC_Style = "NormalCross", LINKS_Name = "Center", } }
+		}
+	},
+	ActiveTool = "ABLayer_Transform"
+}
